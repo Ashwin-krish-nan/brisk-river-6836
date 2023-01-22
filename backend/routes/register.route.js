2f57666586aa994a10ea9c45ac075bb7973cff95
Ashwin-krish-nan <ashwinraj786.as@gmail.com>
2023-01-22 18:25:10 +0530

    completed

--- backend/routes/register.route.js
+++ backend/routes/register.route.js
@@ -11,6 +11,7 @@ register.post("/register",async(req,res)=>{
  try {
    const alreadyusedemail = await Registermodel.find({email})
  if(alreadyusedemail.length>0){
+   res.status(409)
   res.send("Email already in use please login")
  }else{
   bcrypt.hash(password, 5, async(err, hash)=> {
